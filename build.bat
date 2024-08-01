5050d34ef29dd2412c25957653281ecac05c9aa6
Øystein Kolsrud <yko@qlik.com>
2024-08-01 19:58:28 +0200

    More folder name change.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-MSBuild.exe src/QlikOAuthManager/QlikOAuthManager/Qlik.OAuthManager.csproj /t:Build /p:Configuration=Release
+MSBuild.exe src/QlikOAuthManager/Qlik.OAuthManager/Qlik.OAuthManager.csproj /t:Build /p:Configuration=Release
