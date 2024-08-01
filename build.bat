a0a31c8f5120b1383d0656742917eed925d2aa90
Øystein Kolsrud <yko@qlik.com>
2024-08-01 20:06:51 +0200

    Solution name change.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-MSBuild.exe src/QlikOAuthManager/Qlik.OAuthManager/Qlik.OAuthManager.csproj /t:Build /p:Configuration=Release
+MSBuild.exe src/Qlik.OAuthManager/Qlik.OAuthManager/Qlik.OAuthManager.csproj /t:Build /p:Configuration=Release
--- src/Qlik.OAuthManager/Qlik.OAuthManager/VersionAssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-[assembly: System.Reflection.AssemblyVersion("0.0.0.4")]
-[assembly: System.Reflection.AssemblyInformationalVersion("0.0.0.4")]
-[assembly: System.Reflection.AssemblyFileVersion("0.0.0.4")]
