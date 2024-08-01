439e9dd9666f625bf3fd7ad74538782f365dff32
Øystein Kolsrud <yko@qlik.com>
2024-08-01 20:03:15 +0200

    Renamed solutions folder.

---
+++ src/Qlik.OAuthManager/Qlik.OAuthManager/SystemConstants.cs
@@ -0,0 +1,8 @@
+namespace Qlik.OAuthManager
+{
+    public static class SystemConstants
+    {
+        public const string LIBRARY_IDENTIFIER = "Qlik.OAuthManager";
+        public const string LIBRARY_VERSION = "0.0.0.4";
+    }
+}
---
+++ src/Qlik.OAuthManager/Qlik.OAuthManager/VersionAssemblyInfo.cs
@@ -0,0 +1,3 @@
+[assembly: System.Reflection.AssemblyVersion("0.0.0.4")]
+[assembly: System.Reflection.AssemblyInformationalVersion("0.0.0.4")]
+[assembly: System.Reflection.AssemblyFileVersion("0.0.0.4")]
