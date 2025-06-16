f0fbcb821df8d4c3d68da6c639effa96646f82ef
Øystein Kolsrud <yko@qlik.com>
2025-06-16 09:57:53 +0200

    Cosmetic changes only.

--- src/Qlik.OAuthManager/Qlik.OAuthManager/OAuthManager.cs
+++ src/Qlik.OAuthManager/Qlik.OAuthManager/OAuthManager.cs
@@ -13,13 +13,13 @@ using Newtonsoft.Json.Linq;
 namespace Qlik.OAuthManager
 {
 
-	public enum Browser
-	{
-		Default,
-		Chrome,
-		Firefox,
-		MSEdge
-	}
+    public enum Browser
+    {
+        Default,
+        Chrome,
+        Firefox,
+        MSEdge
+    }
 
     public interface IOAuthManager
     {
@@ -44,235 +44,235 @@ namespace Qlik.OAuthManager
     }
 
     public class OAuthManager : IOAuthManager
-	{
-		private const string StyleDefinition = "<style>h1 {text-align: center;}p {text-align: center;}</style>";
-		private const string Body = "<h1>Authentication Complete</h1><p>You can close this tab.<p>";
-		private const string DefaultAuthorizationResponsePage = "<HTML>" + StyleDefinition + "<BODY>" + Body + "</BODY></HTML>";
+    {
+        private const string StyleDefinition = "<style>h1 {text-align: center;}p {text-align: center;}</style>";
+        private const string Body = "<h1>Authentication Complete</h1><p>You can close this tab.<p>";
+        private const string DefaultAuthorizationResponsePage = "<HTML>" + StyleDefinition + "<BODY>" + Body + "</BODY></HTML>";
 
-		public string AuthorizationResponsePage { get; set; } = DefaultAuthorizationResponsePage;
-		public string AccessToken => FullTokenResponse?["access_token"]?.Value<string>();
-		public string RefreshToken => FullTokenResponse?["refresh_token"]?.Value<string>();
-		public JObject FullTokenResponse { get; private set; }
+        public string AuthorizationResponsePage { get; set; } = DefaultAuthorizationResponsePage;
+        public string AccessToken => FullTokenResponse?["access_token"]?.Value<string>();
+        public string RefreshToken => FullTokenResponse?["refresh_token"]?.Value<string>();
+        public JObject FullTokenResponse { get; private set; }
 
-		private readonly HttpClient _httpClient;
+        private readonly HttpClient _httpClient;
 
-		public void AddUserAgent(string productName, string productVersion)
-		{
-			_httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(productName, productVersion));
-		}
+        public void AddUserAgent(string productName, string productVersion)
+        {
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(productName, productVersion));
+        }
 
-		private readonly Uri _tenantUrl;
-		private readonly string _clientId;
-		private readonly Code _code = new Code();
-		private string _authorizationCode;
-		private string _redirectUri;
+        private readonly Uri _tenantUrl;
+        private readonly string _clientId;
+        private readonly Code _code = new Code();
+        private string _authorizationCode;
+        private string _redirectUri;
 
-		public OAuthManager(string tenantUrl, string clientId) : this(new Uri(tenantUrl), clientId)
-		{
-		}
+        public OAuthManager(string tenantUrl, string clientId) : this(new Uri(tenantUrl), clientId)
+        {
+        }
 
-		public OAuthManager(Uri tenantUrl, string clientId)
-		{
-			_tenantUrl = tenantUrl;
-			_clientId = clientId;
-			_httpClient = new HttpClient();
-			AddUserAgent(SystemConstants.LIBRARY_IDENTIFIER, SystemConstants.LIBRARY_VERSION);
-		}
+        public OAuthManager(Uri tenantUrl, string clientId)
+        {
+            _tenantUrl = tenantUrl;
+            _clientId = clientId;
+            _httpClient = new HttpClient();
+            AddUserAgent(SystemConstants.LIBRARY_IDENTIFIER, SystemConstants.LIBRARY_VERSION);
+        }
 
-		public Task AuthorizeInBrowser(string scope, string redirectUri)
-		{
-			return AuthorizeInBrowser(scope, redirectUri, new CancellationToken());
-		}
+        public Task AuthorizeInBrowser(string scope, string redirectUri)
+        {
+            return AuthorizeInBrowser(scope, redirectUri, new CancellationToken());
+        }
 
-		public Task AuthorizeInBrowser(string scope, string redirectUri, Browser browser)
-		{
-			return AuthorizeInBrowser(scope, redirectUri, browser, new CancellationToken());
-		}
+        public Task AuthorizeInBrowser(string scope, string redirectUri, Browser browser)
+        {
+            return AuthorizeInBrowser(scope, redirectUri, browser, new CancellationToken());
+        }
 
-		public Task AuthorizeInBrowser(string scope, string redirectUri, string pathToBrowserExe)
-		{
-			return AuthorizeInBrowser(scope, redirectUri, pathToBrowserExe, new CancellationToken());
-		}
+        public Task AuthorizeInBrowser(string scope, string redirectUri, string pathToBrowserExe)
+        {
+            return AuthorizeInBrowser(scope, redirectUri, pathToBrowserExe, new CancellationToken());
+        }
 
-		public Task AuthorizeInBrowser(string scope, string redirectUri, CancellationToken cancellationToken)
-		{
-			return AuthorizeInBrowser(scope, redirectUri, null, cancellationToken);
-		}
+        public Task AuthorizeInBrowser(string scope, string redirectUri, CancellationToken cancellationToken)
+        {
+            return AuthorizeInBrowser(scope, redirectUri, null, cancellationToken);
+        }
 
-		public Task AuthorizeInBrowser(string scope, string redirectUri, Browser browser, CancellationToken cancellationToken)
-		{
-			return AuthorizeInBrowser(scope, redirectUri, browser == Browser.Default ? null : browser.ToString().ToLower(), cancellationToken);
-		}
+        public Task AuthorizeInBrowser(string scope, string redirectUri, Browser browser, CancellationToken cancellationToken)
+        {
+            return AuthorizeInBrowser(scope, redirectUri, browser == Browser.Default ? null : browser.ToString().ToLower(), cancellationToken);
+        }
 
-		public async Task AuthorizeInBrowser(string scope, string redirectUri, string pathToBrowserExe, CancellationToken cancellationToken)
-		{
-			var state = Guid.NewGuid().ToString();
-			var query = new[]
-			{
-				("response_type", "code"),
-				("client_id", _clientId),
-				("redirect_uri", redirectUri),
-				("scope", scope),
-				("state", state),
-				("code_challenge", _code.CodeChallenge),
-				("code_challenge_method", "S256")
-			};
-			var builder = new UriBuilder(_tenantUrl)
-			{
-				Path = "oauth/authorize",
-				Query = string.Join("&", query.Select(arg => arg.Item1 + "=" + Uri.EscapeDataString(arg.Item2)))
-			};
-			var url = builder.Uri.AbsoluteUri;
-			var processStartInfo = pathToBrowserExe == null
-				? new ProcessStartInfo(builder.Uri.ToString()) { UseShellExecute = true }
-				: new ProcessStartInfo(pathToBrowserExe, url) { UseShellExecute = true };
+        public async Task AuthorizeInBrowser(string scope, string redirectUri, string pathToBrowserExe, CancellationToken cancellationToken)
+        {
+            var state = Guid.NewGuid().ToString();
+            var query = new[]
+            {
+                ("response_type", "code"),
+                ("client_id", _clientId),
+                ("redirect_uri", redirectUri),
+                ("scope", scope),
+                ("state", state),
+                ("code_challenge", _code.CodeChallenge),
+                ("code_challenge_method", "S256")
+            };
+            var builder = new UriBuilder(_tenantUrl)
+            {
+                Path = "oauth/authorize",
+                Query = string.Join("&", query.Select(arg => arg.Item1 + "=" + Uri.EscapeDataString(arg.Item2)))
+            };
+            var url = builder.Uri.AbsoluteUri;
+            var processStartInfo = pathToBrowserExe == null
+                ? new ProcessStartInfo(builder.Uri.ToString()) { UseShellExecute = true }
+                : new ProcessStartInfo(pathToBrowserExe, url) { UseShellExecute = true };
 
-			var callbackHandler = new HttpOAuthCallbackHandler(new Uri(redirectUri), AuthorizationResponsePage);
-			using (Process.Start(processStartInfo))
-			{
-				_authorizationCode = await callbackHandler.GetResponse(cancellationToken).ConfigureAwait(false);
-			}
+            var callbackHandler = new HttpOAuthCallbackHandler(new Uri(redirectUri), AuthorizationResponsePage);
+            using (Process.Start(processStartInfo))
+            {
+                _authorizationCode = await callbackHandler.GetResponse(cancellationToken).ConfigureAwait(false);
+            }
 
-			_redirectUri = redirectUri;
-		}
+            _redirectUri = redirectUri;
+        }
 
-		public Task<string> RequestNewAccessToken()
-		{
-			if (_authorizationCode == null)
-				throw new InvalidOperationException(
-					"Token request must be preceded by authorization call when no client secret is specified.");
+        public Task<string> RequestNewAccessToken()
+        {
+            if (_authorizationCode == null)
+                throw new InvalidOperationException(
+                    "Token request must be preceded by authorization call when no client secret is specified.");
 
-			return RefreshToken == null ? RequestAccessToken() : RefreshAccessToken();
-		}
+            return RefreshToken == null ? RequestAccessToken() : RefreshAccessToken();
+        }
 
-		public async Task<string> RequestNewAccessToken(string clientSecret)
-		{
-			var body = JObject.FromObject(new
-			{
-				scope = "user_default",
-				grant_type = "client_credentials",
-			});
+        public async Task<string> RequestNewAccessToken(string clientSecret)
+        {
+            var body = JObject.FromObject(new
+            {
+                scope = "user_default",
+                grant_type = "client_credentials",
+            });
 
-			FullTokenResponse = await Post("oauth/token", body, clientSecret).ConfigureAwait(false);
-			return AccessToken;
-		}
+            FullTokenResponse = await Post("oauth/token", body, clientSecret).ConfigureAwait(false);
+            return AccessToken;
+        }
 
-		public async Task<string> RequestNewAccessToken(string clientSecret, string subject)
-		{
-			var body = JObject.FromObject(new
-			{
-				scope = "user_default",
-				grant_type = "urn:qlik:oauth:user-impersonation",
-				user_lookup = new
-				{
-					field = "subject",
-					value = subject
-				}
-			});
-			FullTokenResponse = await Post("oauth/token", body, clientSecret).ConfigureAwait(false);
-			return AccessToken;
-		}
+        public async Task<string> RequestNewAccessToken(string clientSecret, string subject)
+        {
+            var body = JObject.FromObject(new
+            {
+                scope = "user_default",
+                grant_type = "urn:qlik:oauth:user-impersonation",
+                user_lookup = new
+                {
+                    field = "subject",
+                    value = subject
+                }
+            });
+            FullTokenResponse = await Post("oauth/token", body, clientSecret).ConfigureAwait(false);
+            return AccessToken;
+        }
 
-		public async Task<string> ExchangeToken(string subjectToken)
-		{
-			var body = JObject.FromObject(new
-			{
-				purpose = "websocket",
-				client_id = _clientId,
-				grant_type = "urn:ietf:params:oauth:grant-type:token-exchange",
-				subject_token = subjectToken,
-				subject_token_type = "urn:ietf:params:oauth:token-type:access_token"
-			});
-			FullTokenResponse = await Post("oauth/token", body).ConfigureAwait(false);
-			return AccessToken;
-		}
+        public async Task<string> ExchangeToken(string subjectToken)
+        {
+            var body = JObject.FromObject(new
+            {
+                purpose = "websocket",
+                client_id = _clientId,
+                grant_type = "urn:ietf:params:oauth:grant-type:token-exchange",
+                subject_token = subjectToken,
+                subject_token_type = "urn:ietf:params:oauth:token-type:access_token"
+            });
+            FullTokenResponse = await Post("oauth/token", body).ConfigureAwait(false);
+            return AccessToken;
+        }
 
-		private async Task<string> RequestAccessToken()
-		{
-			var body = JObject.FromObject(new
-			{
-				client_id = _clientId,
-				code_verifier = _code.CodeVerifier,
-				grant_type = "authorization_code",
-				code = _authorizationCode,
-				redirect_uri = _redirectUri
-			});
+        private async Task<string> RequestAccessToken()
+        {
+            var body = JObject.FromObject(new
+            {
+                client_id = _clientId,
+                code_verifier = _code.CodeVerifier,
+                grant_type = "authorization_code",
+                code = _authorizationCode,
+                redirect_uri = _redirectUri
+            });
 
-			FullTokenResponse = await Post("oauth/token", body).ConfigureAwait(false);
-			return AccessToken;
-		}
+            FullTokenResponse = await Post("oauth/token", body).ConfigureAwait(false);
+            return AccessToken;
+        }
 
-		public async Task<string> RefreshAccessToken(string refreshToken = null)
+        public async Task<string> RefreshAccessToken(string refreshToken = null)
         {
             var token = refreshToken ?? RefreshToken;
-			if (string.IsNullOrEmpty(token))
+            if (string.IsNullOrEmpty(token))
                 throw new InvalidOperationException("No refresh token available.");
 
             var body = JObject.FromObject(new
-			{
-				grant_type = "refresh_token",
-				refresh_token = token
-			});
+            {
+                grant_type = "refresh_token",
+                refresh_token = token
+            });
 
-			FullTokenResponse = await Post("oauth/token", body).ConfigureAwait(false);
-			return AccessToken;
-		}
+            FullTokenResponse = await Post("oauth/token", body).ConfigureAwait(false);
+            return AccessToken;
+        }
 
-		private async Task<JObject> Post(string endpoint, JObject body, string clientSecret = null)
-		{
-			var mContent = new StringContent(body.ToString(), Encoding.ASCII, "application/json");
-			mContent.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
-			var builder = new UriBuilder(_tenantUrl) { Path = endpoint };
-			var message = new HttpRequestMessage(HttpMethod.Post, builder.Uri.AbsoluteUri) { Content = mContent };
-			if (clientSecret != null)
-				message.Headers.Authorization =
-					new AuthenticationHeaderValue("Basic", Base64Encode($"{_clientId}:{clientSecret}"));
-			var rspHttp = await _httpClient.SendAsync(message).ConfigureAwait(false);
-			return JObject.Parse(await rspHttp.Content.ReadAsStringAsync().ConfigureAwait(false));
-		}
+        private async Task<JObject> Post(string endpoint, JObject body, string clientSecret = null)
+        {
+            var mContent = new StringContent(body.ToString(), Encoding.ASCII, "application/json");
+            mContent.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+            var builder = new UriBuilder(_tenantUrl) { Path = endpoint };
+            var message = new HttpRequestMessage(HttpMethod.Post, builder.Uri.AbsoluteUri) { Content = mContent };
+            if (clientSecret != null)
+                message.Headers.Authorization =
+                    new AuthenticationHeaderValue("Basic", Base64Encode($"{_clientId}:{clientSecret}"));
+            var rspHttp = await _httpClient.SendAsync(message).ConfigureAwait(false);
+            return JObject.Parse(await rspHttp.Content.ReadAsStringAsync().ConfigureAwait(false));
+        }
 
-		private static string Base64Encode(string plainText)
-		{
-			var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-			return System.Convert.ToBase64String(plainTextBytes);
-		}
-	}
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
 
-	internal class Code
-	{
-		public readonly string CodeVerifier;
-		public readonly string CodeChallenge;
+    internal class Code
+    {
+        public readonly string CodeVerifier;
+        public readonly string CodeChallenge;
 
-		public Code()
-		{
-			CodeVerifier = GenerateNonce();
-			CodeChallenge = GenerateCodeChallenge(CodeVerifier);
-		}
+        public Code()
+        {
+            CodeVerifier = GenerateNonce();
+            CodeChallenge = GenerateCodeChallenge(CodeVerifier);
+        }
 
-		public static string GenerateNonce()
-		{
-			const string chars = "abcdefghijklmnopqrstuvwxyz123456789";
-			var random = new Random();
-			var nonce = new char[128];
-			for (int i = 0; i < nonce.Length; i++)
-			{
-				nonce[i] = chars[random.Next(chars.Length)];
-			}
+        public static string GenerateNonce()
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz123456789";
+            var random = new Random();
+            var nonce = new char[128];
+            for (int i = 0; i < nonce.Length; i++)
+            {
+                nonce[i] = chars[random.Next(chars.Length)];
+            }
 
-			return new string(nonce);
-		}
+            return new string(nonce);
+        }
 
-		private static string GenerateCodeChallenge(string codeVerifier)
-		{
-			using (var sha256 = SHA256.Create())
-			{
-				var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(codeVerifier));
-				var b64Hash = Convert.ToBase64String(hash);
-				var code = Regex.Replace(b64Hash, "\\+", "-");
-				code = Regex.Replace(code, "\\/", "_");
-				code = Regex.Replace(code, "=+$", "");
-				return code;
-			}
-		}
-	}
+        private static string GenerateCodeChallenge(string codeVerifier)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(codeVerifier));
+                var b64Hash = Convert.ToBase64String(hash);
+                var code = Regex.Replace(b64Hash, "\\+", "-");
+                code = Regex.Replace(code, "\\/", "_");
+                code = Regex.Replace(code, "=+$", "");
+                return code;
+            }
+        }
+    }
 }
